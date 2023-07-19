872604fae7ee10c75d5bdbfb18dfdfc94f0e6610
mariya <mariyakudryashova19980308@gmail.com>
2023-07-19 12:26:49 +0300

    correct02

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,23 +1,18 @@
 ﻿// Напишите программу, которая с помощью деления выводит третью цифру 
 // заданного числа или сообщает, что третьей цифры нет.
 
-
-Random num = new Random ();
-int number = num.Next();
-Console.WriteLine($"Выпало случайное число {number}");
-
- int ThirdDigit(int number)
-        {
-            int result = -1;
-            if (number >= 100)
-            {
-                while (number > 999)
-                {
-                    number = number / 10;
-                }
-                result = number % 10;
-            }
-            return result; 
-        }
-
-Console.WriteLine(result);
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);  // модуль, если вдруг будет введено отрицательное число
+if (number > 99)
+{
+    while(number > 999 )
+    {
+        number /=10;
+    }
+    Console.WriteLine($"Третья цифра числа: {number % 10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}

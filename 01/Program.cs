942cc057e4b7569bada25cdc0a11b9d6c88acf03
mariya <mariyakudryashova19980308@gmail.com>
2023-07-19 11:21:19 +0300

    add01

---
+++ 01/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Random num = new Random ();
+int number = num.Next(100,1000);
+Console.WriteLine($"Выпало случайное число {number}");
+
+int f = number % 100;
+int s = f / 10;
+Console.WriteLine(s);
